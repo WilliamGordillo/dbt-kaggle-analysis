7355a69014773efbc37e973908b43c619ad7f92f
will <will@Xubuntu.myguest.virtualbox.org>
2026-07-14 15:44:31 +0200

    Added tests to staging

--- nyc_taxi_dbt/models/staging/stg_test.sql
+++ nyc_taxi_dbt/models/staging/stg_test.sql
@@ -1,14 +1,9 @@
-{{
-    config(
-    materialized = 'view'
-    )
-}}
 
 
 WITH TEST AS (
-SELECT "VendorID", fare_amount
-FROM {{source('nyc_taxi', 'yellow_tripdata')}}
+SELECT "VendorID" AS  vendorid, "fare_amount"
+FROM {{ source('nyc_taxi', 'yellow_tripdata' )}}
 WHERE fare_amount > 0
 )
 
-SELECT * FROM test limit 100
+SELECT * FROM test
